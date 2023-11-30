8c339749a24f5981842f5c36408d8ca8de775edc
Simona Nikolova <simona.nikolova@vero-digital.solutions>
2023-11-30 16:24:11 +0200

    Remove redundant body

--- android-pdf-viewer/src/main/java/com/github/barteksc/pdfviewer/annotation/core/PdfToImageResultData.kt
+++ android-pdf-viewer/src/main/java/com/github/barteksc/pdfviewer/annotation/core/PdfToImageResultData.kt
@@ -2,5 +2,4 @@ package com.github.barteksc.pdfviewer.annotation.core
 
 import java.io.File
 
-data class PdfToImageResultData(val imageFile: File, val shapes: List<Shape>) {
-}
+data class PdfToImageResultData(val imageFile: File, val shapes: List<Shape>)
