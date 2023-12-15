5a4c540080719237f0dc5b38c3c4a8b57040eee5
Simona Nikolova <simona.nikolova@vero-digital.solutions>
2023-12-15 12:57:41 +0200

    Use annotations to handle polymorphism with Gson

--- android-pdf-viewer/src/main/java/com/github/barteksc/pdfviewer/annotation/core/shapes/Shape.kt
+++ android-pdf-viewer/src/main/java/com/github/barteksc/pdfviewer/annotation/core/shapes/Shape.kt
@@ -2,14 +2,27 @@ package com.github.barteksc.pdfviewer.annotation.core.shapes
 
 import android.graphics.PointF
 import com.github.barteksc.pdfviewer.annotation.core.Annotation
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import com.google.gson.JsonParseException
+import java.lang.reflect.Type
+import kotlin.reflect.KClass
 
-abstract class Shape(@Transient open val type:String, @Transient open val points: List<PointF>)
+@ShapeType(
+    property = "type",
+    subtypes = [
+        ShapeSubtype(clazz = Circle::class, name = "CIRCLE"),
+        ShapeSubtype(clazz = Rectangle::class, name = "RECTANGLE")
+    ]
+)
+abstract class Shape(@Transient open val type: String, @Transient open val points: List<PointF>)
 
-fun Shape.toAnnotation(pageHeight: Int) : Annotation{
-    return when (this){
+fun Shape.toAnnotation(pageHeight: Int): Annotation {
+    return when (this) {
         is Rectangle -> toRectangleAnnotation(pageHeight)
-        is Circle-> toCircleAnnotation(pageHeight)
-        else -> throw Exception ("")
+        is Circle -> toCircleAnnotation(pageHeight)
+        else -> throw Exception("")
     }
 }
 
@@ -24,4 +37,30 @@ fun Shape.toRectangleAnnotation(pageHeight: Int): Annotation {
 fun Shape.toCircleAnnotation(pageHeight: Int): Annotation {
     val points = points.map { it.convertCoordinatesFrom(pageHeight) }
     return Annotation(type = "CIRCLE", points = points)
+}
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.CLASS)
+annotation class ShapeType(val property: String, val subtypes: Array<ShapeSubtype>)
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.CLASS)
+annotation class ShapeSubtype(val clazz: KClass<out Shape>, val name: String)
+
+class ShapeDeserializer : JsonDeserializer<Shape> {
+
+    override fun deserialize(
+        json: JsonElement,
+        typeOfT: Type,
+        context: JsonDeserializationContext
+    ): Shape {
+        val jsonObject = json.asJsonObject
+        val shapeType = jsonObject.get("type").asString
+
+        return when (shapeType) {
+            "CIRCLE" -> context.deserialize(json, Circle::class.java)
+            "RECTANGLE" -> context.deserialize(json, Rectangle::class.java)
+            else -> throw JsonParseException("Unknown shape type: $shapeType")
+        }
+    }
 }
